3360b90c8ff2c977cac4a175ea7ed88c0020c201
Meher <82945138+Meher450@users.noreply.github.com>
2026-01-30 18:10:34 +0530

    Time: 6 ms (69.44%), Space: 13.4 MB (60.54%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,30 +1,61 @@
 class Solution {
 public:
-    bool DFS(vector<vector<int>> &grid,vector<vector<bool>>&visited,int i,int j,int time){
-        int n=grid.size();
-        if(i<0||j<0||i>=n||j>=n||visited[i][j]==true||grid[i][j]>time){
+
+    // DFS to check if destination is reachable at given time
+    bool DFS(vector<vector<int>>& grid,
+             vector<vector<bool>>& visited,
+             int i, int j, int time) {
+
+        int n = grid.size();
+
+        // Base cases:
+        // 1. Out of bounds
+        // 2. Already visited
+        // 3. Cell is above current water level
+        if (i < 0 || j < 0 || i >= n || j >= n ||
+            visited[i][j] == true ||
+            grid[i][j] > time) {
             return false;
         }
-        if(i==(n-1) && j==(n-1)){
+
+        // If destination is reached
+        if (i == n - 1 && j == n - 1) {
             return true;
         }
-        visited[i][j]=true;
 
-        return (DFS(grid,visited,i+1,j,time) || DFS(grid,visited,i,j+1,time) ||
-               DFS(grid,visited,i-1,j,time) || DFS(grid,visited,i,j-1,time));
+        // Mark current cell as visited
+        visited[i][j] = true;
+
+        // Explore all 4 directions
+        return DFS(grid, visited, i + 1, j, time) ||
+               DFS(grid, visited, i - 1, j, time) ||
+               DFS(grid, visited, i, j + 1, time) ||
+               DFS(grid, visited, i, j - 1, time);
     }
+
     int swimInWater(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int low=0, high=n*n;
-        while(low<high){
-            int mid=(high+low)/2;
-            vector<vector<bool>>visited(n,vector<bool>(n,false));
-            if(DFS(grid,visited,0,0,mid)){
-                high=mid;
-            }else{
-                low=mid+1;
+
+        int n = grid.size();
+
+        // Binary search range for time
+        int low = 0, high = n * n;
+
+        while (low < high) {
+
+            int mid = (low + high) / 2;
+
+            // Visited array for DFS
+            vector<vector<bool>> visited(n, vector<bool>(n, false));
+
+            // If path exists at time = mid
+            if (DFS(grid, visited, 0, 0, mid)) {
+                high = mid;   // try smaller time
+            } else {
+                low = mid + 1; // need more time
             }
         }
+
+        // Minimum time required
         return low;
     }
 };
