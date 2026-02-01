73a98da4bc5c75aad9d9e77425d0dd5ef8404c0d
Meher <82945138+Meher450@users.noreply.github.com>
2026-02-01 20:11:46 +0530

    Time: 0 ms (100%), Space: 14.4 MB (88.09%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
-    void DFS(vector<vector<int>>&adj,vector<bool>&vis,int node){
-        vis[node]=true;
-        for(auto &u:adj[node]){
-            if(vis[u]==false){
-                DFS(adj,vis,u);
-            }
-        }
-    }
-    void BFS(vector<vector<int>>&adj,vector<bool>&vis,int node){
-        queue<int>q;
-        q.push(node);
-        vis[node]=true;
-        while(!q.empty()){
-            node=q.front();
-            q.pop();
-            for(auto &u:adj[node]){
-                if(vis[u]==false){
-                    vis[u]=true;
-                    q.push(u);
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        std::vector<bool> visited(rooms.size(), false);
+        size_t rooms_visited = 1;
+        std::vector<int> stack;
+        stack.push_back(0);
+        visited[0] = true;
+
+        while(!stack.empty())
+        {
+            const auto current_room = stack.back();
+            stack.pop_back();
+
+            for(auto adj : rooms[current_room])
+            {
+                if(!visited[adj])
+                {
+                    ++rooms_visited;
+                    visited[adj] = true;
+                    stack.emplace_back(adj);
                 }
             }
         }
-    }
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int n=rooms.size();
-        vector<vector<int>>adj(n);
-        vector<bool>vis(n,false);
-        for(int i=0;i<n;i++){
-            for(auto &u:rooms[i]){
-                adj[i].push_back(u);
-            }
-        }
-        //DFS(adj,vis,0);
-        BFS(adj,vis,0);
-        for(int i=0;i<n;i++){
-            if(vis[i]==false) return false;
-        }
-        return true;
+        
+
+        return rooms_visited == rooms.size();
     }
 };
