48183bae267a7ea2cedd3b701f58b199f1f3d59e
Meher <82945138+Meher450@users.noreply.github.com>
2026-02-05 10:57:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int atMostDistinct(vector<int> &arr,int k){
+        vector<int>freq(128,0);
+        int n=arr.size(), si=0, ei=0, count=0, subArrayCount=0;
+
+        while(ei<n){
+            if(freq[arr[ei++]]++ == 0) count++;
+            while(count > k){
+                if(freq[arr[si++]]-- == 1) count--;
+            }
+            subArrayCount += ei-si;
+        }
+        return subArrayCount;
+    }
+    int subarraysWithKDistinct(vector<int>& arr, int k) {
+        return atMostDistinct(arr,k) - atMostDistinct(arr,k-1);
+    }
+};
